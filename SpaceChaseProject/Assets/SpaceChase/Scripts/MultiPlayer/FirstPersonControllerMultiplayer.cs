12c6d8e797914a23704d87ac0ac71c3912c65e59
Pilegrim <pilegrim60@gmail.com>
2022-10-19 18:25:21 +0200

    prepped project for multiplayer

---
+++ SpaceChaseProject/Assets/SpaceChase/Scripts/MultiPlayer/FirstPersonControllerMultiplayer.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(GravityBody))]
+public class FirstPersonControllerMultiplayer : MonoBehaviour
+{
+    // public vars
+    public float mouseSensitivityY = 1;
+    public float walkSpeed = 6;
+    public float jumpForce = 220;
+    public LayerMask groundedMask;
+    public SceneManagerMultiplayer sceneManager;
+
+    // System vars
+    bool grounded = true;
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    float verticalLookRotation;
+    Transform cameraTransform;
+    Rigidbody rigidbody;
+
+
+    void Awake()
+    {
+        sceneManager = GameObject.FindWithTag("SceneManager").GetComponent<SceneManagerMultiplayer>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraTransform = Camera.main.transform;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (sceneManager.IsPlaying())
+        {
+                  
+            verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+            verticalLookRotation = Mathf.Clamp(verticalLookRotation, -40, -10);
+            cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+        
+        
+            // Calculate movement:
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float inputY = Input.GetAxisRaw("Vertical");
+
+            Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+            Vector3 targetMoveAmount = moveDir * walkSpeed;
+            moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+
+            // Jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (grounded)
+                {
+                    rigidbody.AddForce(transform.up * jumpForce);
+                    grounded = false;
+                }
+            }
+        }
+  
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Planet"))
+        {
+            grounded = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (sceneManager.IsPlaying())
+        {
+            // Apply movement to rigidbody
+            Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+            rigidbody.MovePosition(rigidbody.position + localMove);
+        }
+
+    }
+}
