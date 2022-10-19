7ae94a08b6b101b20e259885f43c2dc6233caa46
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-10-19 19:40:08 +0200

    worked on minimap

--- SpaceChaseProject/Assets/SpaceChase/Scripts/Minimap.cs
+++ SpaceChaseProject/Assets/SpaceChase/Scripts/Minimap.cs
@@ -5,12 +5,43 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-    public Transform player;
+    public Transform gamePlanetsParent;
+    public Transform gamePlayer;
+    public GameObject miniPlanetsPrefab;
+    public GameObject miniPlayerPrefab;
+    public Transform minimapPosition;
 
-    private void LateUpdate()
+    private List<Transform> gamePlanets = new List<Transform>();
+    private List<Transform> miniPlanets;
+
+    void Start()
     {
-        Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
+        for (int i = 0; i < gamePlanetsParent.childCount; i++)
+        {
+            gamePlanets.Add(gamePlanetsParent.GetChild(i));
+        }
+        
+        // create mini planets at correct position in ui
+        foreach (Transform gamePlanet in gamePlanets)
+        {
+            var miniPlanet = Instantiate(miniPlanetsPrefab);
+            miniPlanet.transform.localPosition = gamePlanet.localPosition;
+            miniPlanet.transform.parent = transform;
+        }
+        
+        // create mini player at correct position in ui
+        var miniPlayer = Instantiate(miniPlayerPrefab);
+        miniPlayer.transform.localPosition = gamePlayer.localPosition;
+        miniPlayer.transform.parent = transform;
+        
+        
+        
+        transform.localScale *= 0.006f;
     }
+
+    private void Update()
+    {
+        transform.position = minimapPosition.position;
+    }
+    
 }
